7ccc42fe7aefa6a66b2b1074ded33a55310013f0
Pongpang-2102 <puriwat_sangrawee@hotmail.com>
2022-01-23 02:02:57 +0700

    input 2 number for math operation
    first version for program  which number will be input by 2 number to be operated together (x,y) by +,-,*,/

---
+++ Excercise5_1_P.py
@@ -0,0 +1,7 @@
+print("Please input 2 numbers ")
+x=float(input("please input 1st number : "))
+y=float(input("please input 2nd number : "))
+print(x,"+",y,"=",x+y)
+print(x,"-",y,"=",x-y)
+print(x,"x",y,"=",x*y)
+print(x,"/",y,"=",x/y)
---
+++ Excercise5_2_Puriwat_S.py
@@ -0,0 +1,5 @@
+print("This is Velocity Calculation Program")
+s=float(input("Please input distance or S(km)"))
+t=float(input("Please input duration(hr)"))
+v=s/t
+print("from Distance (km)=",s,"," ,"time (hr)=",t,",", "speed will be :",v,"km/hr")
