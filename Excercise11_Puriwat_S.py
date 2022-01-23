5fcaf5d5bd015198e978a68c65d80a6afde91e15
Pongpang-2102 <puriwat_sangrawee@hotmail.com>
2022-01-24 00:36:12 +0700

    Create Excercise11_Puriwat_S.py
    the asterisk pyramid will be displayed based on input

---
+++ Excercise11_Puriwat_S.py
@@ -0,0 +1,10 @@
+
+print("U can draw pyramid of asterisk as big as you needed")
+AsteriskNo=int(input("Please specify number : "))
+
+for y in range(AsteriskNo) :
+      print("*"*((2*y)+1))
+
+
+
+
