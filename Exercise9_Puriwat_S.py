65a62c7b9cc53516c07f231ac7e369e19226ccaa
Pongpang-2102 <puriwat_sangrawee@hotmail.com>
2022-01-23 18:10:10 +0700

    Create Exercise9_Puriwat_S.py

---
+++ Exercise9_Puriwat_S.py
@@ -0,0 +1,9 @@
+UsernameInput=input("Username : ")
+PasswordInput=input("Password : ")
+while not (UsernameInput == 'admin' and PasswordInput == '1234'):
+    UsernameInput = input("Username : ")
+    PasswordInput = input("Password : ")
+
+print("Welcome !! System is Starting")
+
+
