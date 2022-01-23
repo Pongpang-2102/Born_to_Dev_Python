2b57dd268b49b886aae6cfbb0ff8405b3ce28ca5
Pongpang-2102 <puriwat_sangrawee@hotmail.com>
2022-01-24 01:38:53 +0700

    Create Homework_Lecture50_Puriwat_S.py
    Trial program for  Defining function for number basic operation(+,-,*,/)

---
+++ Homework_Lecture50_Puriwat_S.py
@@ -0,0 +1,42 @@
+def Add(x,y):
+    print("This is Add Function")
+    print(x,"+",y,"=",x+y)
+    print("-----------")
+def Deduct(x,y):
+    print("This is Deduct Function")
+    print(x,"-",y,"=",x-y)
+    print("-----------")
+def Multiply(x,y):
+    print("This is Multiply Function")
+    print(x,"x",y,"=",x*y)
+    print("-----------")
+def Devide(x,y):
+    print("This is Devide Function")
+    print(x,"/",y,"=",x/y)
+    print("-----------")
+
+def Combine (x,y):
+    print("This is Combine (+-*/) Function")
+    print(x, "+", y, "=", x + y)
+    print(x, "-", y, "=", x - y)
+    print(x, "x", y, "=", x * y)
+    print(x, "/", y, "=", x / y)
+    print("-----------")
+
+Add(3,7)
+Deduct(3,7)
+Multiply(3,7)
+Devide(3,7)
+Combine(3,7)
+
+
+
+
+
+
+
+
+
+
+#   num=int(input())
+ #  num+=1
